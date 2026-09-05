e2a0451f661b27d1426adc4e4814c9c21cef2792
DanielJohn17 <danielyohannes38@gmail.com>
2026-09-05 21:35:08 +0300

    added parse json and validation

--- app/internal/api/helpers/helpers.go
+++ app/internal/api/helpers/helpers.go
@@ -2,9 +2,45 @@
 package helpers
 
 import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
 	"github.com/DanielJohn17/tui-chat/app/internal/api/types"
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
+
+// ParseJSON transforms json byte slice into go struct
+func ParseJSON[K comparable](c *gin.Context, payload *K) *types.APIErrorResponse {
+	validate := validator.New()
+
+	if c.Request.Body == nil {
+		return &types.APIErrorResponse{
+			Status:  http.StatusBadRequest,
+			Success: false,
+			Message: "missing request body",
+		}
+	}
+
+	if err := json.NewDecoder(c.Request.Body).Decode(payload); err != nil {
+		return &types.APIErrorResponse{
+			Status:  http.StatusBadRequest,
+			Success: false,
+			Message: fmt.Errorf("error decoding payload: %w", err).Error(),
+		}
+	}
+
+	if err := validate.Struct(payload); err != nil {
+		return &types.APIErrorResponse{
+			Status:  http.StatusBadRequest,
+			Success: false,
+			Message: fmt.Errorf("validation error: %w", err).Error(),
+		}
+	}
+
+	return nil
+}
 
 func WriteJSON[T comparable](c *gin.Context, response types.APIResponse[T]) {
 	c.IndentedJSON(response.Status, gin.H{
